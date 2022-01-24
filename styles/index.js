c2205dd3781afeb3c483a9985ff507ef0593eb9a
tepeszCodes <tomek.szymanderski@gmail.com>
2022-01-24 19:12:31 +0100

    added basic typography styles and updated filename

--- styles/index.js
+++ styles/index.js
@@ -1,7 +1,7 @@
 import * as button from "./button";
 import * as colors from "./colors";
 import * as cornerRadius from "./cornerRadius";
-import * as text from "./text";
+import * as text from "./textColors";
 import * as textInput from "./textInput";
 
 export { button, colors, cornerRadius, text, textInput };
---
+++ styles/typography.js
@@ -0,0 +1,59 @@
+export const heading1 = {
+  fontSize: 36,
+  fontWeight: "bold",
+};
+
+export const heading2 = {
+  fontSize: 28,
+  fontWeight: "bold",
+};
+
+export const heading3 = {
+  fontSize: 22,
+  fontWeight: "bold",
+};
+
+export const heading4 = {
+  fontSize: 16,
+  fontWeight: 600,
+};
+
+export const buttonText = {
+  fontSize: 16,
+  fontWeight: 600,
+  letterSpacing: "1%",
+};
+
+export const label = {
+  fontSize: 16,
+  fontWeight: 500,
+};
+
+export const titleAndInput = {
+  fontSize: 15,
+  fontWeight: 500,
+  lineHeight: 20,
+};
+
+export const captionPoppins = {
+  fontSize: 10,
+  fontWeight: 400,
+  lineHeight: 10,
+};
+
+export const bodyText = {
+  fontSize: 14,
+  fontWeight: 400,
+};
+
+export const captionCompact = {
+  fontSize: 12,
+  fontWeight: "bold",
+  lineHeight: 16,
+};
+
+export const specialText = {
+  fontSize: 12,
+  fontWeight: 400,
+  lineHeight: 16,
+};
