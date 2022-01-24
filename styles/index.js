c7bfb6ca6a888fe04c8c31c49902677511bc511c
tepeszCodes <tomek.szymanderski@gmail.com>
2022-01-24 18:38:41 +0100

    implemented barrel pattern for base styles

---
+++ styles/index.js
@@ -0,0 +1,7 @@
+import * as button from "./button";
+import * as colors from "./colors";
+import * as cornerRadius from "./cornerRadius";
+import * as text from "./text";
+import * as textInput from "./textInput";
+
+export { button, colors, cornerRadius, text, textInput };
--- styles/textInput.js
+++ styles/textInput.js
@@ -1,3 +1,5 @@
+import { white } from "./colors";
+
 export const basicTextInput = {
   width: "100%",
   borderRadius: 12,
