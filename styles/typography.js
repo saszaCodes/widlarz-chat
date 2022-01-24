032f2802bd4b371482c692937dc1e30d366bc42b
tepeszCodes <tomek.szymanderski@gmail.com>
2022-01-24 19:14:34 +0100

    (bug fix) changes font weights to strings

--- styles/typography.js
+++ styles/typography.js
@@ -15,35 +15,35 @@ export const heading3 = {
 
 export const heading4 = {
   fontSize: 16,
-  fontWeight: 600,
+  fontWeight: "600",
 };
 
 export const buttonText = {
   fontSize: 16,
-  fontWeight: 600,
+  fontWeight: "600",
   letterSpacing: "1%",
 };
 
 export const label = {
   fontSize: 16,
-  fontWeight: 500,
+  fontWeight: "500",
 };
 
 export const titleAndInput = {
   fontSize: 15,
-  fontWeight: 500,
+  fontWeight: "500",
   lineHeight: 20,
 };
 
 export const captionPoppins = {
   fontSize: 10,
-  fontWeight: 400,
+  fontWeight: "400",
   lineHeight: 10,
 };
 
 export const bodyText = {
   fontSize: 14,
-  fontWeight: 400,
+  fontWeight: "400",
 };
 
 export const captionCompact = {
@@ -54,6 +54,6 @@ export const captionCompact = {
 
 export const specialText = {
   fontSize: 12,
-  fontWeight: 400,
+  fontWeight: "400",
   lineHeight: 16,
 };
